693377543d9b38854fcf432b3947b0a781d170dd
Manoj Paidimarri <manoj.paidimarri@epicinfotech.com>
2017-10-19 21:24:56 -0400

    created tf file

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example"  {
+        ami = "ami-4fffc834"
+        instance_type = "t2.micro"
+        security_groups = ["allow_all"]
+        tags {
+        Name="terraform-instance"
+        }
+}
